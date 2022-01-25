3f06f29a5e7ebadf61e5c168c21e16e4afeb7b7c
TangChangXin <592609672@qq.com>
2022-01-25 12:52:14 +0800

    有效的括号

--- Stack_lc20.cpp
+++ Stack_lc20.cpp
@@ -3,7 +3,6 @@
 //
 
 #include <string>
-#include <iostream>
 #include <unordered_map>
 #include <stack>
 using namespace std;
@@ -30,5 +29,28 @@ public:
             }
         }
         return true; //默认是全部配对的
+    }
+
+    //2022年1月25日。细节没想清楚
+    bool 有效括号(string s) {
+        if (s.size() % 2 == 1) return false;
+        stack<char> f辅助栈;
+        //判断右括号是否匹配左括号，左括号需要入栈。
+        unordered_map<char, char> k括号对 = {
+                {')', '('},
+                {']', '['},
+                {'}', '{'}
+        };
+        for (char c : s) {
+            if (k括号对.count(c)) {
+                 //加上“f辅助栈.empty()”的判断是为了应对第一个字符就是右括号，这样直接就返回false。但此时栈是空的，第一个不能判断。
+                if (f辅助栈.top() != k括号对[c] || f辅助栈.empty())
+                    return false;
+                f辅助栈.pop();
+            } else {
+                f辅助栈.push(c);
+            }
+        }
+        return true;
     }
 };
