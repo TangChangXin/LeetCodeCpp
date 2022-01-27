b8dadec9bc3b18f44b02c096c38d1bc60468f625
TangChangXin <592609672@qq.com>
2022-01-27 17:38:55 +0800

    删除链表的倒数第N个结点

--- DualPointer_lc19.cpp
+++ DualPointer_lc19.cpp
@@ -7,6 +7,21 @@
 #include "Linkedlist_lc2.cpp"
 class Solution19 {
 public:
+    //2022年1月27日，链表至少有一个节点，所以头结点不为空
+    ListNode *题解(ListNode* head, int n) {
+        //这样写无需对头节点进行额外分析。
+        ListNode *q前置节点 = new ListNode(0, head), *k快指针 = q前置节点, *m慢指针 = q前置节点;
+        for (int i = 0; i < n; ++i) {
+            k快指针 = k快指针->next;
+        }
+        while (k快指针->next != nullptr) {
+            k快指针 = k快指针->next;
+            m慢指针 = m慢指针->next;
+        }
+        m慢指针->next = m慢指针->next->next;
+        return q前置节点->next;
+    }
+
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         if (head == nullptr || head->next == nullptr) return nullptr;
         ListNode *x虚拟头 = new ListNode(0, head), *m慢指针 = x虚拟头, *k快指针 = x虚拟头;
