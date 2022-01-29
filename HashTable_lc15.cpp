4ae78ddac4465636a5ce039e75bf21de1627b869
TangChangXin <592609672@qq.com>
2022-01-29 11:35:22 +0800

    三数之和

--- HashTable_lc15.cpp
+++ HashTable_lc15.cpp
@@ -3,12 +3,14 @@
 //
 
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
 class Solution15 {
 public:
     vector<vector<int>> threeSum(vector<int> &nums) {
-
+        int s数组长度 = nums.size();
+        vector<vector<int>> j结果;
+        if (s数组长度 < 3) return j结果;
     }
 };
