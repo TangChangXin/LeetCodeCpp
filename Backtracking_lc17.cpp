4acffe9b2b4dd8bb8e5c9aba3ab00e219fd541e8
TangChangXin <592609672@qq.com>
2022-01-30 15:24:04 +0800

    电话号码的字母组合

--- Backtracking_lc17.cpp
+++ Backtracking_lc17.cpp
@@ -13,6 +13,31 @@ class Solution17 {
 public:
     vector<string> j结果;
     string l路径;
+    //2022年1月30日，哈希表的键定义错了，开始定义成了int。没考虑空数字串的情况
+    unordered_map<char, string> z字母映射表 = {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"},
+    };
+
+    void h回溯函数2(string s数字串, int q起始位置) {
+        if (l路径.size() == s数字串.size()) {
+            j结果.push_back(l路径);
+            return;
+        }
+
+        //本来在这个外面又套了一层循环，但实际不需要，在递归时就隐含了对数字的循环遍历。
+        for (char z字母: z字母映射表[s数字串[q起始位置]]) {
+            l路径.push_back(z字母);
+            h回溯函数2(s数字串,q起始位置 + 1);
+            l路径.pop_back();
+        }
+    }
 
     //第一次做时用到的数字和字母对应表
     string y映射表[10] = {
@@ -59,7 +84,7 @@ public:
 
         char s数字 = s数字串[q起始位置];
         string z字母 = s数字字母映射表.at(s数字);
-        for (auto c:z字母) {
+        for (auto c: z字母) {
             l路径.push_back(c); //做出选择，将数字对应的字母加入路径中
             h回溯函数(s数字串, q起始位置 + 1); //递归遍历下一个数字
             l路径.pop_back(); //撤销选择，删除字母
