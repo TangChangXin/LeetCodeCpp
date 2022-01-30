133ed766634129263d07eb1437f8fbe3aeb30673
TangChangXin <592609672@qq.com>
2022-01-30 09:46:29 +0800

    组合

--- Backtracking_lc77.cpp
+++ Backtracking_lc77.cpp
@@ -3,38 +3,53 @@
 //
 
 #include <vector>
+
 using namespace std;
+
 class Solution77 {
 public:
     vector<vector<int>> j结果;
     vector<int> l路径;
+
     //2022年1月29日
+    void h回溯剪枝(int n, int k, int q起始位置) {
+        if (l路径.size() == k) {
+            j结果.push_back(l路径);
+            return;
+        }
+        //TODO 剪枝的结束条件写错了
+        for (int i = q起始位置; i <= n - (k - l路径.size()) + 1; ++i) {
+            l路径.push_back(i);
+            h回溯剪枝(n, k, i + 1);
+            l路径.pop_back();
+        }
+    }
 
     //没有剪枝的回溯法  第一次做
-    void h回溯函数(int n, int k, int q起始数字){
-        if (l路径.size() == k){
+    void h回溯函数(int n, int k, int q起始数字) {
+        if (l路径.size() == k) {
             j结果.push_back(l路径); //当前的路径中已经包含了满足条件的k个数，将路径加入结果中。
             return;//进入下一次递归
         }
 
         for (int i = q起始数字; i <= n; ++i) {
             l路径.push_back(i); //将当前访问的数字加入到路径数组中。
-            h回溯函数(n, k, i+1);
+            h回溯函数(n, k, i + 1);
             l路径.pop_back(); //撤销做出的选择
         }
     }
 
     //剪枝的回溯法  第一次做
-    void j剪枝回溯(int n, int k, int q起始数字){
-        if (l路径.size() == k){
+    void j剪枝回溯(int n, int k, int q起始数字) {
+        if (l路径.size() == k) {
             j结果.push_back(l路径); //当前的路径中已经包含了满足条件的k个数，将路径加入结果中。
             return;//进入下一次递归
         }
 
         //从起始数字开始，如果剩余元素个数不满足我们需要的元素个数，不必再继续搜索
-        for (int i = q起始数字; i <= n-(k-l路径.size())+1; ++i) {
+        for (int i = q起始数字; i <= n - (k - l路径.size()) + 1; ++i) {
             l路径.push_back(i); //将当前访问的数字加入到路径数组中。
-            h回溯函数(n, k, i+1);
+            h回溯函数(n, k, i + 1);
             l路径.pop_back(); //撤销做出的选择
         }
     }
