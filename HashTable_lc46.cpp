732ec4c08b3abc44fb7b6903e63c2d6594a53d30
TangChangXin <592609672@qq.com>
2022-02-05 11:59:26 +0800

    全排列

--- HashTable_lc46.cpp
+++ HashTable_lc46.cpp
@@ -10,26 +10,26 @@ class Solution46 {
 public:
     vector<vector<int>> q全排列结果;
     vector<int> p排列;
-    int y已访问[21] = {0};
 
     //之前用Java写过
-    void h回溯函数(vector<int> &w无重复数组) {
+    void h回溯函数(vector<int> &w无重复数组, vector<bool> &y已访问) {
         if (p排列.size() == w无重复数组.size()) {
             q全排列结果.push_back(p排列);
             return;
         }
         for (int i = 0; i < w无重复数组.size(); ++i) {
-            if (y已访问[w无重复数组[i] + 10] == 1) continue;
+            if (y已访问[w无重复数组[i] + 10] == true) continue;
             p排列.push_back(w无重复数组[i]);
-            y已访问[w无重复数组[i] + 10] = 1;
-            h回溯函数(w无重复数组);
+            y已访问[w无重复数组[i] + 10] = true;
+            h回溯函数(w无重复数组, y已访问);
             p排列.pop_back();
-            y已访问[w无重复数组[i] + 10] = 0;
+            y已访问[w无重复数组[i] + 10] = false;
         }
     }
     
     vector<vector<int>> permute(vector<int> &nums) {
-        h回溯函数(nums);
+        vector<bool> y已访问位置(nums.size(), false);
+        h回溯函数(nums, y已访问位置);
         return q全排列结果;
     }
 };
