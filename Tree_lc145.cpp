986123c69c3a385afaf42c810640b9cabd6c3789
TangChangXin <592609672@qq.com>
2022-02-09 20:35:17 +0800

    二叉树的后序遍历

--- Tree_lc145.cpp
+++ Tree_lc145.cpp
@@ -7,34 +7,49 @@
 #include <stack>
 #include "Tree_lc94.cpp"
 
-
-//2021 10 21 第二次
-vector<int> 迭代法后序遍历(TreeNode *root) {
-    vector<int> b遍历结果;
-    stack<TreeNode *> m模拟递归栈;
-    if (root == nullptr)
-        return b遍历结果;
-    else m模拟递归栈.push(root);
-
-    while (!m模拟递归栈.empty()) {
-        TreeNode *d当前节点 = m模拟递归栈.top(); //取栈顶元素
-        m模拟递归栈.pop(); //移除栈顶元素
-        b遍历结果.push_back(d当前节点->val);
-        if (d当前节点->left) m模拟递归栈.push(d当前节点->left); //注意这里，左子结点后访问就先入栈
-        if (d当前节点->right) m模拟递归栈.push(d当前节点->right); //右子结点先访问就后入栈
-    }
-    reverse(b遍历结果.begin(), b遍历结果.end());
-    return b遍历结果;
-}
-
-
-
-
-
-
-
 class Solution145 {
 public:
+    //2022年2月9日
+    void d递归法后序(TreeNode *g根节点, vector<int> &h后序结果) {
+        if (g根节点 == nullptr) return;
+        d递归法后序(g根节点->left, h后序结果);
+        d递归法后序(g根节点->right, h后序结果);
+        h后序结果.push_back(g根节点->val);
+    }
+
+    //2022年2月9日
+    vector<int> d迭代法后序(TreeNode *g根节点) {
+        vector<int> j结果;
+        if (g根节点 == nullptr) return j结果;
+        stack<TreeNode *> m模拟栈;
+        m模拟栈.push(g根节点);
+        while (!m模拟栈.empty()) {
+            TreeNode *d当前节点 = m模拟栈.top();
+            j结果.push_back(d当前节点->val);
+            m模拟栈.pop();
+            if (d当前节点->left) m模拟栈.push(d当前节点->left);
+            if (d当前节点->right) m模拟栈.push(d当前节点->right);
+        }
+        reverse(j结果.begin(),  j结果.end());
+        return j结果;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // 采用根右左的方式遍历树并保存到暂存栈1中，然后暂存栈1元素出栈再压入到暂存栈2中。此时暂存栈2中的顺序即为左右根
     // 暂存栈2再出栈并保存到返回数组，这样遍历得到的数组就是后序的左右根。看的哪个题解搞忘了。
     vector<int> 双栈法(TreeNode *root) {
