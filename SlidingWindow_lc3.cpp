9b52156e44a498e38625a73674ff07091a8f5f88
TangChangXin <592609672@qq.com>
2022-03-02 19:06:32 +0800

    无重复字符的最长子串

--- SlidingWindow_lc3.cpp
+++ SlidingWindow_lc3.cpp
@@ -22,9 +22,29 @@ using namespace std;
 //中等 无重复字符的最长子串
 class Solution3 {
 public:
+    //2022年3月2日，路西法的方法，有小细节不好理解。
+    int lengthOfLongestSubstring2(string s) {
+        int w无重复字符的最长子串长度 = 0;
+        int q起始位置 = 0, z字符串长度 = s.size();
+        unordered_map<char, int> z字符位置; //这个哈希表中保存的是每个正在被访问的字符对应的索引
+        for (int i = 0; i < z字符串长度; ++i) {
+            //如果哈希表中已经存在字符了，那么就将重复字符的下一个字符作为新的子串起点
+            char z正被访问字符 = s[i];
+            if (z字符位置.count(z正被访问字符))
+                q起始位置 = max(q起始位置, z字符位置[z正被访问字符] + 1);
+                //todo 如果字符串是“abccba”，轴对称式重复，试一下就理解了。起始位置被设置为重复字符的前一个字符位置，范围扩大了。
+                // q起始位置 = z字符位置[z正被访问字符] + 1;
+
+            //如果不存在，那么当前字符对应的位置是子串的结束位置，加入哈希表中
+            z字符位置[z正被访问字符] = i;
+            w无重复字符的最长子串长度 = max(w无重复字符的最长子串长度, i - q起始位置 + 1);
+        }
+        return w无重复字符的最长子串长度;
+    }
+
     int lengthOfLongestSubstring(string s) {
         if (s.empty()) return 0;
-        unordered_map<char, int> c窗口;
+        unordered_map<char, int> c窗口; //值保存的是出现过的次数
 
         int z左边界 = 0, y右边界 = 0, c长度 = 0;
         while (y右边界 < s.size()){
