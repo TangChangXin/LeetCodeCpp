a92d246208b38b03c1ae948c1f1845505b503097
TangChangXin <592609672@qq.com>
2022-03-03 16:38:21 +0800

    最大子数组和

--- Greedy_lc53.cpp
+++ Greedy_lc53.cpp
@@ -7,6 +7,29 @@ using namespace std;
 
 class Solution53 {
 public:
+    //2022年3月3日,有些小瑕疵
+    int z最大子数组和(vector<int>& nums) {
+        int j结果 = INT_MIN, s数组和 = 0;
+        for (int num : nums) {
+            s数组和 += num;
+            j结果 = max(j结果, s数组和);
+            if (s数组和 <= 0)
+                s数组和 = 0;
+        }
+        return j结果;
+        /*
+        int j结果 = INT_MIN, s数组和 = 0;
+        for (int num : nums) {
+            s数组和 += num;
+            if (s数组和 > j结果)
+                j结果 = s数组和;
+            if (s数组和 <= 0)
+                s数组和 = 0;
+        }
+        return j结果;
+         */
+    }
+
     //第一次做，看了随想录
     int maxSubArray(vector<int>& nums) {
         int z最大子序和 = INT_MIN, l临时和 = 0, s数组长度 = nums.size();
