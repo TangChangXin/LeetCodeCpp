653924b886f6646126b06a842558b4fec5649aed
TangChangXin <592609672@qq.com>
2022-03-23 14:27:04 +0800

    合并两个有序链表

--- DualPointer_lc21.cpp
+++ DualPointer_lc21.cpp
@@ -5,6 +5,27 @@
 #include "Linkedlist_lc2.cpp"
 class Solution21 {
 public:
+    //2022年3月23日
+    ListNode* mergeTwoLists2(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr && list2 == nullptr) return nullptr;
+        ListNode *x新头节点 = new ListNode(0, nullptr), *d当前节点 = x新头节点;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val >= list2->val) {
+                d当前节点->next = list2;
+                list2 = list2->next;
+            } else {
+                d当前节点->next = list1;
+                list1 = list1->next;
+            }
+            d当前节点 = d当前节点->next;
+        }
+        if (list1 == nullptr)
+            d当前节点->next = list2;
+        else
+            d当前节点->next = list1;
+        return x新头节点->next;
+    }
+
     //迭代法
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         if (l1 == nullptr && l2 == nullptr) return nullptr;
