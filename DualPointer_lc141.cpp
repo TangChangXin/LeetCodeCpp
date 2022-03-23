3b751f4c310ee8859e15dfba8f751a2ae285f9d6
TangChangXin <592609672@qq.com>
2022-03-23 16:47:47 +0800

    环形链表

--- DualPointer_lc141.cpp
+++ DualPointer_lc141.cpp
@@ -5,6 +5,18 @@
 #include "Linkedlist_lc2.cpp"
 class Solution141 {
 public:
+    bool hasCycle2(ListNode *head) {
+        ListNode *m慢指针 = head, *k快指针 = head;
+        while (k快指针 != nullptr && k快指针->next != nullptr) {
+            m慢指针 = m慢指针->next;
+            k快指针 = k快指针->next->next;
+            if (m慢指针 == k快指针)
+                return true;
+        }
+        //默认没有环
+        return false;
+    }
+
     bool hasCycle(ListNode *head) {
         //有瑕疵
         ListNode *k快指针 = head, *m慢指针 = head;
