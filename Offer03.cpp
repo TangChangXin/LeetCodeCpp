aa5e737be269aa589fb23851b68871aaf8d676b4
TangChangXin <592609672@qq.com>
2022-03-24 10:43:32 +0800

    剑指 Offer 03. 数组中重复的数字

--- Offer03.cpp
+++ Offer03.cpp
@@ -7,6 +7,21 @@ using namespace std;
 
 class Solution03 {
 public:
+    //2022年3月24日
+    int findRepeatNumber2(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            // 如果“i等于nums[i]”那么不需要操作
+            while (i != nums[i]) {
+                //如果相等，说明重复的数字就是nums[i]
+                if (nums[i] == nums[nums[i]])
+                    return nums[i];
+                //不相等就交换两个数字，通过不断的交换令nums[i]出现在位置i处
+                swap(nums[i], nums[nums[i]]);
+            }
+        }
+        return -1;
+    }
+
     //2022年3月8日
     int findRepeatNumber(vector<int> &nums) {
         for (int i = 0; i < nums.size(); ++i) {
