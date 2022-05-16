5397612125d2ab37e97bc99a5f905b2a4c5c1571
TangChangXin <592609672@qq.com>
2022-05-16 10:53:24 +0800

    CMake文件

---
+++ Prefix_lc560.cpp
@@ -0,0 +1,16 @@
+//
+// Created by XK on 2022/5/16.
+//
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    //2022年5月16日第一次做,看了labuladong的题解
+    int subarraySum(vector<int> &nums, int k) {
+        unordered_map<int, int> 前缀和表; //定义一个哈希表保存每个前缀和结果出现的次数
+
+    }
+};
