a236d2d97471d1a987e16c49e028b4556da8b128
TangChangXin <592609672@qq.com>
2022-05-18 20:27:39 +0800

    四数相加 II

--- HashTable_lc454.cpp
+++ HashTable_lc454.cpp
@@ -5,7 +5,7 @@
 #include <unordered_map>
 using namespace std;
 
-class Solution {
+class Solution454 {
 public:
     int fourSumCount(vector<int> &nums1, vector<int> &nums2, vector<int> &nums3, vector<int> &nums4) {
         unordered_map<int, int> s数组和映射;
